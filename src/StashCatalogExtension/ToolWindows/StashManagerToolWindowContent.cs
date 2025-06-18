12dd7eef6cdeb2fa4587378409c58bd15f3e3208
Damian Ubowski <damian.ubowski.0@gmail.com>
2025-06-18 15:03:26 +0200

    feature: add empty stashes window

---
+++ src/StashCatalogExtension/ToolWindows/StashManagerToolWindowContent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.Extensibility.UI;
+
+namespace StashCatalogExtension.ToolWindows
+{
+    /// <summary>
+    /// A remote user control to use as tool window UI content.
+    /// </summary>
+    internal class StashManagerToolWindowContent : RemoteUserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StashManagerToolWindowContent" /> class.
+        /// </summary>
+        public StashManagerToolWindowContent()
+            : base(dataContext: new StashManagerToolWindowData())
+        {
+        }
+    }
+}
