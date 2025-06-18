903bef0f0aad96e99737ba1c0e16b831185db926
Damian Ubowski <damian.ubowski.0@gmail.com>
2025-06-18 14:06:35 +0200

    chore: Use VisualStudio.Extensibility Project template

---
+++ src/StashCatalogExtension/ExtensionEntrypoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace StashCatalogExtension
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                    id: "StashCatalogExtension.f6251360-c7c3-4a5b-8c0c-892d106f087e",
+                    version: this.ExtensionAssemblyVersion,
+                    publisherName: "Publisher name",
+                    displayName: "StashCatalogExtension",
+                    description: "Extension description"),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}
